e4d423e41c8d359fde5ef0695dd45798395718e0
Rmalnoult <romainmalnoult@gmail.com>
2014-11-24 16:14:11 +0100

    fixed user registration

--- app/cache/dev/twig/b5/96/2ba1b19f2f413d81f5ca942b19f7099975e5802c1ac97ab46eeef599c58e.php
+++ app/cache/dev/twig/b5/96/2ba1b19f2f413d81f5ca942b19f7099975e5802c1ac97ab46eeef599c58e.php
@@ -28,72 +28,7 @@ class __TwigTemplate_b5962ba1b19f2f413d81f5ca942b19f7099975e5802c1ac97ab46eeef59
     public function block_fos_user_content($context, array $blocks = array())
     {
         // line 4
-        echo "    <body>
-    <p>hello register</p>
-        <div>
-            ";
-        // line 7
-        if ($this->env->getExtension('security')->isGranted("IS_AUTHENTICATED_REMEMBERED")) {
-            // line 8
-            echo "                ";
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.logged_in_as", array("%username%" => $this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "user", array()), "username", array())), "FOSUserBundle"), "html", null, true);
-            echo " |
-                <a href=\"";
-            // line 9
-            echo $this->env->getExtension('routing')->getPath("fos_user_security_logout");
-            echo "\">
-                    ";
-            // line 10
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.logout", array(), "FOSUserBundle"), "html", null, true);
-            echo "
-                </a>
-            ";
-        } else {
-            // line 13
-            echo "                <a href=\"";
-            echo $this->env->getExtension('routing')->getPath("fos_user_security_login");
-            echo "\">";
-            echo twig_escape_filter($this->env, $this->env->getExtension('translator')->trans("layout.login", array(), "FOSUserBundle"), "html", null, true);
-            echo "</a>
-            ";
-        }
-        // line 15
-        echo "        </div>
-
-        ";
-        // line 17
-        $context['_parent'] = (array) $context;
-        $context['_seq'] = twig_ensure_traversable($this->getAttribute($this->getAttribute($this->getAttribute((isset($context["app"]) ? $context["app"] : $this->getContext($context, "app")), "session", array()), "flashbag", array()), "all", array(), "method"));
-        foreach ($context['_seq'] as $context["type"] => $context["messages"]) {
-            // line 18
-            echo "            ";
-            $context['_parent'] = (array) $context;
-            $context['_seq'] = twig_ensure_traversable($context["messages"]);
-            foreach ($context['_seq'] as $context["_key"] => $context["message"]) {
-                // line 19
-                echo "                <div class=\"flash-";
-                echo twig_escape_filter($this->env, $context["type"], "html", null, true);
-                echo "\">
-                    ";
-                // line 20
-                echo twig_escape_filter($this->env, $context["message"], "html", null, true);
-                echo "
-                </div>
-            ";
-            }
-            $_parent = $context['_parent'];
-            unset($context['_seq'], $context['_iterated'], $context['_key'], $context['message'], $context['_parent'], $context['loop']);
-            $context = array_intersect_key($context, $_parent) + $_parent;
-            // line 23
-            echo "        ";
-        }
-        $_parent = $context['_parent'];
-        unset($context['_seq'], $context['_iterated'], $context['type'], $context['messages'], $context['_parent'], $context['loop']);
-        $context = array_intersect_key($context, $_parent) + $_parent;
-        // line 24
-        echo "
-    </body>
-";
+        $this->env->loadTemplate("FOSUserBundle:Registration:register_content.html.twig")->display($context);
     }
 
     public function getTemplateName()
@@ -108,6 +43,6 @@ class __TwigTemplate_b5962ba1b19f2f413d81f5ca942b19f7099975e5802c1ac97ab46eeef59
 
     public function getDebugInfo()
     {
-        return array (  94 => 24,  88 => 23,  79 => 20,  74 => 19,  69 => 18,  65 => 17,  61 => 15,  53 => 13,  47 => 10,  43 => 9,  38 => 8,  36 => 7,  31 => 4,  28 => 3,);
+        return array (  31 => 4,  28 => 3,);
     }
 }
